f1a6438eceef47b993389c5c46bdaff259095f22
Jun Lee <dan.junwoo.lee@gmail.com>
2024-02-19 10:25:45 -0500

    update dashboard

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -3,28 +3,46 @@ import { Link } from "react-router-dom";
 import { courses } from "../Database";
 import "./index.css";
 
-function Dashboard({setTitle}: {
-  setTitle: React.Dispatch<React.SetStateAction<string>>
+function Dashboard({
+  setTitle,
+}: {
+  setTitle: React.Dispatch<React.SetStateAction<string>>;
 }) {
   setTitle("");
   return (
     <div className="wd-kanbas-dashboard-main">
-      <h1>Dashboard</h1>              <hr />
-      <h2>Published Courses (12)</h2> <hr />
+      <h1>Dashboard</h1>
+      <hr />
+      <h2>Published Courses ({courses.length})</h2> <hr />
       <div className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
           {courses.map((course) => (
             <div key={course._id} className="col" style={{ width: 300 }}>
               <div className="card">
-                <img src={`/images/${course.image}`} className="card-img-top"
-                     style={{ height: 150 }}/>
+                <img
+                  src={`/images/${course.image}`}
+                  className="card-img-top"
+                  style={{ height: 150 }}
+                />
                 <div className="card-body">
-                  <Link className="card-title" to={`/Kanbas/Courses/${course._id}/Home`}
-                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                    {course.name} </Link>
+                  <Link
+                    className="card-title"
+                    to={`/Kanbas/Courses/${course._id}/Home`}
+                    style={{
+                      textDecoration: "none",
+                      color: "navy",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {course.name}{" "}
+                  </Link>
                   <p className="card-text">{course.name}</p>
-                  <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">
-                    Go </Link>
+                  <Link
+                    to={`/Kanbas/Courses/${course._id}/Home`}
+                    className="btn btn-primary"
+                  >
+                    Go{" "}
+                  </Link>
                 </div>
               </div>
             </div>
