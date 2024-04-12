048bf6530df2999aca89ffc7c0f5cac38986b046
Jun Lee <dan.junwoo.lee@gmail.com>
2024-04-11 20:22:20 -0400

    credentials

--- src/Kanbas/Users/client.ts
+++ src/Kanbas/Users/client.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 export const BASE_API = process.env.REACT_APP_API_BASE;
 export const USERS_API = `${BASE_API}/api/users`;
+
+axios.defaults.withCredentials = true;
 export interface User { _id: string; username: string; password: string; role: string;
 firstName: string, lastName: string };
 export const signin = async (credentials: User) => {
