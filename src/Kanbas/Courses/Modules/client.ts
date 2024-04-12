ed55d2f95224220acd805ab44c08dcaf20e673c8
Jun Lee <dan.junwoo.lee@gmail.com>
2024-04-11 20:28:49 -0400

    credentials attempt 2

--- src/Kanbas/Courses/Modules/client.ts
+++ src/Kanbas/Courses/Modules/client.ts
@@ -3,21 +3,23 @@ const API_BASE = process.env.REACT_APP_API_BASE;
 const COURSES_API = `${API_BASE}/api/courses`;
 const MODULES_API = `${API_BASE}/api/modules`;
 
-axios.defaults.withCredentials = true;
+const api = axios.create({
+  withCredentials: true
+})
 
 export const updateModule = async (module: any) => {
-    const response = await axios.
-      put(`${MODULES_API}/${module._id}`, module);
+    const response = await api
+      .put(`${MODULES_API}/${module._id}`, module);
     return response.data;
   };
   
 export const deleteModule = async (moduleId: string) => {
-  const response = await axios.delete(`${MODULES_API}/${moduleId}`);
+  const response = await api.delete(`${MODULES_API}/${moduleId}`);
   return response.data;
 };
 
 export const createModule = async (courseId: string, module: any) => {
-  const response = await axios.post(
+  const response = await api.post(
     `${COURSES_API}/${courseId}/modules`,
     module
   );
@@ -25,6 +27,6 @@ export const createModule = async (courseId: string, module: any) => {
 };
 
 export const findModulesForCourse = async (courseId: string) => {
-  const response = await axios.get(`${COURSES_API}/${courseId}/modules`);
+  const response = await api.get(`${COURSES_API}/${courseId}/modules`);
   return response.data;
 };
